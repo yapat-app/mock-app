f65a470d4917e9a94825ea7fe1d4e0cbdaa9a3ea
Thiago <thiago.gouvea@dfki.de>
2025-02-13 19:06:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">AquaMorph is Live!</h1>
+    </div>
+  );
+}
+
+export default App;
